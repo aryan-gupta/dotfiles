65bfc8e4d55b691ce9a22dac0d8128eb3e0da915
Aryan Gupta <me@theguptaempire.net>
2024-02-29 22:28:03 -0500

    Update polybar laptop config for consistency

--- .config/polybar/start.sh
+++ .config/polybar/start.sh
@@ -4,12 +4,19 @@
 export ROOT_FS_TEXT="%{F$COLOR_ACNTLGT_N}root%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage_used:2%% "
 export HOME_FS_TEXT="%{F$COLOR_ACNTLGT_N}data%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage_used:2%% "
 export NAS_FS_TEXT="%{F$COLOR_ACNTLGT_N}nas%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage_used:2%% "
-export ROOT_FS_TEXT_ERR="%{F$COLOR_ACNTLGT_N}root%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}!MNT%{F-} "
-export HOME_FS_TEXT_ERR="%{F$COLOR_ACNTLGT_N}data%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}!MNT%{F-} "
-export NAS_FS_TEXT_ERR="%{F$COLOR_ACNTLGT_N}nas%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}!MNT%{F-} "
+export ROOT_FS_TEXT_ERR="%{F$COLOR_ACNTLGT_N}root%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}!mnt%{F-} "
+export HOME_FS_TEXT_ERR="%{F$COLOR_ACNTLGT_N}data%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}!mnt%{F-} "
+export NAS_FS_TEXT_ERR="%{F$COLOR_ACNTLGT_N}nas%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}!mnt%{F-} "
 export TEMP_TEXT="%{F$COLOR_ACNTLGT_N}temp%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %temperature-c% "
 export CPU_TEXT="%{F$COLOR_ACNTLGT_N}cpu%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage-sum:3% "
 export MEM_TEXT="%{F$COLOR_ACNTLGT_N}mem%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage_used:2%% "
+export VOL_TEXT="%{F$COLOR_ACNTLGT_N}vol%{F-}%{F$COLOR_ACNTDRK_L}:%{F-}%percentage:3%% "
+export VOM_TEXT="%{F$COLOR_ACNTLGT_N}vol%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}mut%{F-} "
+export BL_TEXT="%{F$COLOR_ACNTLGT_N}blite%{F-}%{F$COLOR_ACNTDRK_L}:%{F-}%percentage:3%% "
+export BATT_C_TEXT="%{F$COLOR_ACNTLGT_N}batt%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage:2%% "
+export BATT_D_TEXT="%{F$COLOR_ACNTLGT_N}batt%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %percentage:2%% "
+export BATT_F_TEXT="%{F$COLOR_ACNTLGT_N}batt%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_L}ful%{F-} "
+export BATT_E_TEXT="%{F$COLOR_ACNTLGT_N}batt%{F-}%{F$COLOR_ACNTDRK_L}:%{F-} %{F$COLOR_WARNING_H}low%{F-} "
 export MUSIC_TEXT="%title%%{F$COLOR_ACNTDRK_L} - %{F-}%{F$COLOR_ACNTLGT_N}%artist%%{F-}"
 export DATE_TEXT="%Y%{F$COLOR_ACNTDRK_L}-%{F-}%m%{F$COLOR_ACNTDRK_L}-%{F-}%d"
 export TIME_TEXT="%H%{F$COLOR_ACNTDRK_L}:%{F-}%M"
@@ -19,4 +26,5 @@ polybar ws &
 # polybar ws-tv &
 [ $POLYBAR_MUSIC = "True" ] && polybar music &
 polybar sysinfo &
+polybar sysinfo-laptop &
 polybar ws-flags &
